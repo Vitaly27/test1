1607b24fad68de9051fba400b59c1ffcf411e131
Vitaly27 <50526160+Vitaly27@users.noreply.github.com>
2019-05-12 20:43:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,6 @@ bot = telebot.TeleBot("835209382:AAFeY1RmWlpq0l35z2W0ywgYnre1-GSX9AA")
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
-    bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, "Здравствуйте, на данном этапе я пока не понимаю Вас! Но всё изменится!")
 
 bot.polling( none_stop = True)
